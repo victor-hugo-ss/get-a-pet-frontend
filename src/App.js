4fe2341079facd4526d9e6123a593af52741c622
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-19 11:35:30 -0300

    feat: estrutura inicial das páginas home, login e register

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+/* Páginas */
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
+import Home from './components/pages/Home';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Get a Pet</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
