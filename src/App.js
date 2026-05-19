a3c3e669759b15ff2c12bd4d3e5cb7a94210c320
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-19 12:05:30 -0300

    feat: inicio de criação do navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+/* Components */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 /* Páginas */
 import Login from './components/pages/auth/Login';
 import Register from './components/pages/auth/Register';
@@ -8,11 +12,15 @@ import Home from './components/pages/Home';
 function App() {
     return (
         <BrowserRouter>
+            <Navbar />
+
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/" element={<Home />} />
             </Routes>
+
+            <Footer />
         </BrowserRouter>
     );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                <span className="bold">Get A Pet</span> &copy; 2026
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
