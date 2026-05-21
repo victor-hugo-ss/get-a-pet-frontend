bccd8ca5a9175723751cc683b8c1d30b9002a71d
victor-hugo-ss <victorabreu2512@gmail.com>
2026-05-21 12:19:36 -0300

    feat: criação de formulário de login

--- src/components/pages/auth/Login.js
+++ src/components/pages/auth/Login.js
@@ -1,7 +1,38 @@
+import { useState, useContext } from 'react';
+import Input from '../../form/Input';
+
+import styles from '../../form/Form.module.css';
+
+// Context
+import { Context } from '../../../context/UserContext';
+import { Link } from 'react-router-dom';
+
 function Login() {
+    function handleChange() {}
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu e-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Entrar" />
+            </form>
+            <p>
+                Não tem conta? <Link to="/register">Clique aqui.</Link>
+            </p>
         </section>
     );
 }
